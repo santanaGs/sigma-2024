748fa792a4b5bfcf043ac11ac06c17814842d787
gabriel <gabrielsantana2267@gmail.com>
2024-03-24 21:27:19 -0300

    :sparkles: feat: Add nav bar by: Vinicius

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Login from "@/screens/Login";
 import Main from "@/screens/Main";
+import Agenda from "@/screens/Agenda";
+import Suporte from "@/screens/Suporte";
+import Prontuario from "@/screens/Prontuario";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,10 +12,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Main />} />
+          <Route path="/agenda" element={<Agenda />} />
+          <Route path="/suporte" element={<Suporte />} />
+          <Route path="/prontuario" element={<Prontuario />} />
         </Routes>
       </Router>
     </>
   );
 }
-
 export default App;
---
+++ src/screens/Agenda/index.tsx
@@ -0,0 +1,11 @@
+import NavBar from "@/components/NavBar";
+
+const Agenda: React.FC = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Agenda;
